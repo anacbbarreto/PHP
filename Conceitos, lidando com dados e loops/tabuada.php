e9fe4928c375401be2bbaafbd1a727106cad83fd
anacbbarreto <anacbbarreto@yahoo.com.br>
2022-06-28 16:47:58 -0300

    Tabuada - criar de 1 até 9

---
+++ Conceitos, lidando com dados e loops/tabuada.php
@@ -0,0 +1,11 @@
+<?php
+
+$numero = 2;
+
+
+for ($contador = 1; $contador < 10; $contador ++) {
+      $total = $numero * $contador;
+           echo "A multiplicação de $numero * $contador é: $total" . PHP_EOL;
+           
+      } 
+      
