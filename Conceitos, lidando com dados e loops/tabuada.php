a5c83e8b3a9877c25e58c1846f1b1a9c2aee741e
anacbbarreto <anacbbarreto@yahoo.com.br>
2022-06-28 16:59:47 -0300

    Calculadora de IMC

--- Conceitos, lidando com dados e loops/tabuada.php
+++ Conceitos, lidando com dados e loops/tabuada.php
@@ -5,7 +5,7 @@ $numero = 2;
 
 for ($i = 1; $i < 10; $i ++) {
       $total = $numero * $i;
-           echo "A multiplicação de $numero * $i é: $total" . PHP_EOL;
+           echo " $numero * $i : $total" . PHP_EOL;
            
       } 
       
