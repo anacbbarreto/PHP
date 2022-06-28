20c2a077c0c48bfd636fe08b759ec46dd787954f
anacbbarreto <anacbbarreto@yahoo.com.br>
2022-06-28 12:00:42 -0300

    decisoes, if e elseif

---
+++ Conceitos, lidando com dados e loops/tipos.php
@@ -0,0 +1,12 @@
+<?php
+
+$idade =28; //inteiro
+ $salario  =100.01; //decimal
+ $divisão = 97/4; //divisão
+ $texto = "olá"; //texto
+ $verdadeiro = true; // boolean verdadeiro
+ $falso = false; //boolean falso
+ 
+ 
+ echo $divisão; // imprime a variavel
+ echo gettype($salario); // verifica o tipo da variavel
