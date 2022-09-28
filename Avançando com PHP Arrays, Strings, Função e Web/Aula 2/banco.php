ca1e6902450fa5bf1cd568d9d09cc561829aca77
Ana Carolina Brandão Barreto <ana.barreto@voxline.com.br>
2022-09-28 12:49:34 -0300

    aula 8

--- Avançando com PHP Arrays, Strings, Função e Web/Aula 2/banco.php
+++ Avançando com PHP Arrays, Strings, Função e Web/Aula 2/banco.php
@@ -1,11 +1,40 @@
 <?php
 
+
+// uma subrotina executa tudo o que precisa e nao retorna valor nenhum
+function exibeMensagem($mensagem){
+    echo $mensagem . PHP_EOL;
+}
+
+// uma função executa tudo o que precisa e retorna valor 
+
+function sacar(array $conta, float $valorASacar): array
+{
+    if ($valorASacar > $conta['saldo']) {
+        exibeMensagem("Você não pode sacar este valor");
+    } else {
+        $conta['saldo'] -= $valorASacar;
+    }
+
+    return $conta;
+}
+
+function depositar(array $conta, float $valorADepositar): array {
+    if ($valorADepositar > 0) {
+        $conta['saldo'] += $valorADepositar;
+    } else {
+        exibeMensagem("Depósitos precisam ser positivos");
+    }
+
+    return $conta;
+}
+
 $contasCorrentes = [
     '123.456.789-10' => [
         'titular' => 'Maria',
         'saldo' => 10000
     ],
-    '123.456.689-11' => [
+    '123.456.789-11' => [
         'titular' => 'Alberto',
         'saldo' => 300
     ],
@@ -15,21 +44,26 @@ $contasCorrentes = [
     ]
 ];
 
-if (500 > $contasCorrentes['123.456.789-10']['saldo']) {
-    exibeMensagem("Você não pode sacar este valor");
-} else
-    $contasCorrentes['123.456.789-10']['saldo'] -= 500;
+
+//add o cpf da conta que deseja fazer a função
+$contasCorrentes['123.456.789-11'] = sacar($contasCorrentes['123.456.789-11'], 500);
+
+$contasCorrentes['123.256.789-12'] = depositar(
+    $contasCorrentes['123.256.789-12'],
+    900
+);
 
 
-if (500 > $contasCorrentes['123.456.789-11']['saldo']) {
-    exibeMensagem("Você não pode sacar este valor");
-} else
-    $contasCorrentes['123.456.789-11']['saldo'] -= 500;
+$contasCorrentes['123.456.789-10'] = sacar(
+    $contasCorrentes['123.456.789-10'],
+    500
+);
+
+$contasCorrentes['123.456.789-11'] = sacar(
+    $contasCorrentes['123.456.789-11'], 
+    200
+);
 
 foreach ($contasCorrentes as $cpf => $conta) {
     exibeMensagem($cpf . " " . $conta['titular'] . ' ' . $conta['saldo']);
-}
-
-function exibeMensagem($mensagem){
-    echo $mensagem . PHP_EOL;
 }
