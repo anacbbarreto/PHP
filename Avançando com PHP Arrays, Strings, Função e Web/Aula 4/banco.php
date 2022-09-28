0204f90bc7b6c00f02cfbc185a363f352a7799f4
Ana Carolina Brandão Barreto <ana.barreto@voxline.com.br>
2022-09-28 17:50:49 -0300

    Fim do 4° modulo

--- Avançando com PHP Arrays, Strings, Função e Web/Aula 4/banco.php
+++ Avançando com PHP Arrays, Strings, Função e Web/Aula 4/banco.php
@@ -1,33 +1,19 @@
 <?php
 
+/**
+ * para incluir o arquivo porem se o arquivo nao existir, apresenta apenas uma mensagem 
+ * include 'funcoes.php';
+ */
 
-// uma subrotina executa tudo o que precisa e nao retorna valor nenhum
-function exibeMensagem($mensagem){
-    echo $mensagem . PHP_EOL;
-}
+/**
+ * arquivo é obrigatorio e nao compila
+ *require 'funcoes.php';
+ */
 
-// uma função executa tudo o que precisa e retorna valor 
 
-function sacar(array $conta, float $valorASacar): array
-{
-    if ($valorASacar > $conta['saldo']) {
-        exibeMensagem("Você não pode sacar este valor");
-    } else {
-        $conta['saldo'] -= $valorASacar;
-    }
+// verifica se o arquivo já foi importado antes.
+require_once 'funcoes.php';
 
-    return $conta;
-}
-
-function depositar(array $conta, float $valorADepositar): array {
-    if ($valorADepositar > 0) {
-        $conta['saldo'] += $valorADepositar;
-    } else {
-        exibeMensagem("Depósitos precisam ser positivos");
-    }
-
-    return $conta;
-}
 
 $contasCorrentes = [
     '123.456.789-10' => [
@@ -64,7 +50,18 @@ $contasCorrentes['123.456.789-11'] = sacar(
     200
 );
 
+//strings simples e strings complexas.
+
+//strings simples
 foreach ($contasCorrentes as $cpf => $conta) {
     exibeMensagem(
-                  $cpf . " " . $conta['titular'] . ' ' . $conta['saldo']);
+        "$cpf $conta[titular] $conta[saldo]"
+    );
+}
+
+//strings complexas para acessar o array da mesma forma de utilizamos.
+foreach ($contasCorrentes as $cpf => $conta) {
+    exibeMensagem(
+        "$cpf {$conta['titular']} {$conta['saldo']}"
+    );
 }
