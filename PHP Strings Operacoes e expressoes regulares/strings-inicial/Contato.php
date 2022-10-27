d693d72b5ea2137a41c332594584b6861232de5d
Ana Carolina Brandão Barreto <ana.barreto@voxline.com.br>
2022-10-27 10:32:54 -0300

    expressoes regulares, Utilizar a expressao preg_match para aplicar uma expressao regular e Validar um telefone a partir de uma expressao regular.

--- PHP Strings Operacoes e expressoes regulares/strings-inicial/Contato.php
+++ PHP Strings Operacoes e expressoes regulares/strings-inicial/Contato.php
@@ -1,26 +1,35 @@
 <?php
 
 namespace App\Alura;
+
 class Contato
 {
     private $email;
     private $endereco;
     private $cep;
+    private $telefone;
 
-    public function __construct(string $email, string $endereco, string $cep)
-    {
-        $this->email = $email;
+    public function __construct(string $email, string $endereco, string $cep, string $telefone)
+{
+    $this->email = $email;
 
-        if ($this->validaEmail($email) !== false) {
-            $this->setEmail($email);
-        } else {
-            $this->setEmail("Email inválido");
-        }
-
-        $this->endereco = $endereco;
-        $this->cep = $cep;
+    if ($this->validaEmail($email) !== false) {
+        $this->setEmail($email);
+    } else {
+        $this->setEmail("Email inválido");
     }
-    
+
+    if ($this->validaTelefone($telefone)) {
+        $this->setTelefone($telefone);
+    } else {
+        $this->setTelefone("Telefone inválido");
+    }
+
+    $this->endereco = $endereco;
+    $this->cep = $cep;
+}
+
+   
     public function getUsuario(): string
     {
         $posicaoArroba = strpos($this->email, "@");
@@ -48,6 +57,27 @@ public function getEnderecoCep(): string
     return implode(" - ", $enderecoCep);
 }
 
+}
+
+public function getTelefone(): string
+{
+    return $this->telefone;
+}
+
+private function validaTelefone(string $telefone): int
+{
+    //6455-7546
+    return preg_match('/^[0-9]{4}-[0-9]{4}$/', $telefone, $encontrados);
+}
+
+private function setEmail(string $email): void
+{
+    $this->email = $email;
+}
+
+private function setTelefone(string $telefone): void
+{
+    $this->telefone = $telefone;
 }
 
 ?>
--- PHP Strings Operacoes e expressoes regulares/strings-inicial/cadastro.php
+++ PHP Strings Operacoes e expressoes regulares/strings-inicial/cadastro.php
@@ -5,9 +5,9 @@ use App\Alura\Usuario;
 
 require 'autoload.php';
 
-$usuario = new Usuario($_POST['nome']);
-$contato = new Contato($_POST['email'], $_POST['endereco'], $_POST['cep']);
 $usuario = new Usuario($_POST['nome'], $_POST['senha']);
+$contato = new Contato($_POST['email'], $_POST['endereco'], $_POST['cep'], $_POST['telefone']);
+
 
 ?>
 
@@ -30,7 +30,7 @@ $usuario = new Usuario($_POST['nome'], $_POST['senha']);
     <li class="list-group-item">Sobrenome: <?php echo $usuario->getSobrenome() ?></li>
     <li class="list-group-item">Usuário: <?php echo $contato->getUsuario() ?></li>
     <li class="list-group-item">Senha: <?php echo $usuario->getSenha() ?></li>
-    <li class="list-group-item">Telefone: </li>
+    <li class="list-group-item">Telefone: <?php echo $contato->getTelefone() ?></li>
     <li class="list-group-item">Email: <?php echo $contato->getEmail() ?> </li>
     <li class="list-group-item">Endereço: <?php echo $contato->getEnderecoCep() ?></li>
 </ul>
