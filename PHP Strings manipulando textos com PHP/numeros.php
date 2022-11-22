54d4cee9c0a5f255d005c88267e4aef5b80ce6dc
Ana Carolina Brandão Barreto <ana.barreto@voxline.com.br>
2022-11-22 17:44:19 -0300

    numeros e strings

---
+++ PHP Strings manipulando textos com PHP/numeros.php
@@ -0,0 +1,18 @@
+<?php
+
+$anoNascimento = '1994';
+
+$idade = 2022 - $anoNascimento;
+
+echo $idade.PHP_EOL;
+
+if($anoNascimento == 1994){
+echo 'SIM'.PHP_EOL;
+} else{
+    echo 'NÃO'.PHP_EOL;
+
+}
+
+
+ 
+?>
