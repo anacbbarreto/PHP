930956c8265f937ec3e65ffa4bc26b493736486c
Ana Carolina Brandão Barreto <ana.barreto@voxline.com.br>
2024-09-30 18:26:40 -0300

    Add versao

--- PHP Composer Dependencias, Autoload e Publicacao/aula 5/buscar-cursos.php
+++ PHP Composer Dependencias, Autoload e Publicacao/aula 5/buscar-cursos.php
@@ -7,7 +7,7 @@ use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
 $client = new Client(['base_uri' => 'https://www.alura.com.br/']);
-$crawler = new Crawler();
+$crawler = new Crawler ();
 
 $buscardor = new Buscador($client, $crawler);
 $cursos = $buscardor->buscar('/cursos-online-programacao/php');
